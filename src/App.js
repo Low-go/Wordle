e86badd78632956c9ca7ec62001a7bbed41cce0e
Low-go <lorranalvesgaldino98@gmail.com>
2025-06-15 20:41:00 -1000

    proxy installed to circumvent cors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 
-const API_URL = 'https://api.frontendexpert.io/api/fe/wordle-words'
+const API_URL = '/api/fe/wordle-words';
 
 
 export default function App(){
@@ -11,10 +11,15 @@ export default function App(){
     useEffect(() => {
 
         const fetchWord = async () => { // outer function cannot be async
+
+           
             const response = await fetch(API_URL); // but inner function can
             const words = await response.json();
+            console.log('fetchWord function called');
+
             const randomWord = words[Math.floor(Math.random() * words.length)]
             setSolution(randomWord);
+            console.log(randomWord)
         };
 
         fetchWord();
